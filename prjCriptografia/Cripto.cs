db9e53d2f07c6e0edf3e73fdb2be2f7106aabf1f
Caio Scura <86533634+CaioScura@users.noreply.github.com>
2023-07-04 02:41:03 -0300

    Add files via upload

---
+++ prjCriptografia/Cripto.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Security.Cryptography;
+
+namespace prjCriptografia
+{
+    public class Cripto
+    {
+        const string chave = "12345";
+
+        public static string criptografar(string texto)
+        {
+            byte[] Results;
+
+            // gerar o HASH
+            UTF8Encoding UTF8 = new UTF8Encoding();
+            MD5CryptoServiceProvider HashProvider = new MD5CryptoServiceProvider();
+            byte[] TDESKey = HashProvider.ComputeHash(UTF8.GetBytes(chave));
+
+            // criptografar o texto ....
+            TripleDESCryptoServiceProvider algoritmo = new TripleDESCryptoServiceProvider();
+            algoritmo.Key = TDESKey;
+            algoritmo.Mode = CipherMode.ECB;
+            algoritmo.Padding = PaddingMode.PKCS7;
+
+            byte[] DataToEncrpty = UTF8.GetBytes(texto);
+
+            try
+            {
+                ICryptoTransform encriptador = algoritmo.CreateEncryptor();
+                Results = encriptador.TransformFinalBlock(DataToEncrpty, 0,
+                    DataToEncrpty.Length);
+                return Convert.ToBase64String(Results);
+            }
+
+            catch (Exception Erro)
+            {
+                algoritmo.Clear();
+                HashProvider.Clear();
+                Console.WriteLine("Erro: {0}", Erro.Message);
+            }
+
+            return "";
+        }
+
+
+        public static string descriptografar(string texto)
+        {
+            byte[] Results;
+
+            // gerar o HASH
+            UTF8Encoding UTF8 = new UTF8Encoding();
+            MD5CryptoServiceProvider HashProvider = new MD5CryptoServiceProvider();
+            byte[] TDESKey = HashProvider.ComputeHash(UTF8.GetBytes(chave));
+            TripleDESCryptoServiceProvider algoritmo = new TripleDESCryptoServiceProvider();
+
+            algoritmo.Key = TDESKey;
+            algoritmo.Mode = CipherMode.ECB;
+            algoritmo.Padding = PaddingMode.PKCS7;
+            byte[] DataToDecrpty = Convert.FromBase64String(texto);
+
+            try
+            {
+                ICryptoTransform decriptador = algoritmo.CreateDecryptor();
+                Results = decriptador.TransformFinalBlock(DataToDecrpty, 0,
+                    DataToDecrpty.Length);
+
+                return UTF8.GetString(Results);
+            }
+
+            catch (Exception Erro)
+            {
+                algoritmo.Clear();
+                HashProvider.Clear();
+                Console.WriteLine("Erro: {0}", Erro.Message);
+            }
+
+            return "";
+        }
+    }
+}
---
+++ prjCriptografia/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjCriptografia
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string Texto = "";
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("----- TESTE DE CRIPTOGRAFIA DE DADOS DA API --------");
+            Console.Write("DIGITE O TEXTO A SER CRIPTOGRAFADO: ");
+
+            Texto = Console.ReadLine();
+
+            string Saida = Cripto.criptografar(Texto);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nTEXTO CRIPTOGRAFADO:{0}", Saida);
+            Console.WriteLine("\n----- TESTE DE DESCRIPTOGRAFIA DE DADOS DA API --------");
+
+            string Saida2 = Cripto.descriptografar(Saida);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("TEXTO DECRIPTOGRAFADO:{0}", Saida2);
+            Console.ReadKey();
+        }
+    }
+}
